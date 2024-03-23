92fab1b1d2ae3f3881f2ac105d60329e5be88ab0
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-03-23 12:22:50 +0100

    add typing using worldeboard

--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Svg from "../assets/Svg";
 import { chevronBack } from "../helpers/svgs";
 import { motion } from "framer-motion";
@@ -8,6 +8,45 @@ import { alphabetinquerty, getrandomword } from "../helpers/utilities";
 export default function Wordlepage() {
   const tries = 6;
   const [word, setWord] = useState(getrandomword());
+  const [acceptedletters, setAcceptedletters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currenttry, setCurrenttry] = useState({
+    word: "",
+    tries: 0,
+  });
+  useEffect(() => {
+    setWord(getrandomword());
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const wordleboard = document.getElementById("wordleboard");
+    if (!wordleboard) return;
+    wordleboard.addEventListener("click", (e) => {
+      const target = e.target as HTMLElement;
+      if (target.innerText === "⌫") {
+        setCurrenttry({
+          word: currenttry.word.slice(0, -1),
+          tries: currenttry.tries,
+        });
+      } else if (target.innerText === "Enter") {
+        if (currenttry.word === word) {
+          console.log("You won");
+        } else {
+          setCurrenttry({
+            word: "",
+            tries: currenttry.tries + 1,
+          });
+        }
+      } else {
+        setCurrenttry({
+          word: currenttry.word + target.innerText,
+          tries: currenttry.tries,
+        });
+      }
+    });
+  }, [currenttry, word]);
+
   return (
     <main className="h-screen flex flex-col items-center justify-around relative">
       <Link
@@ -23,51 +62,52 @@ export default function Wordlepage() {
         />
         return
       </Link>
-      <div className="flex flex-col gap-8 items-center">
-        <div className="text-pink-50 text-7xl ">Wordle Game</div>
-        <div
-          className="flex flex-col gap-4 
+      {!loading && (
+        <div className="flex flex-col gap-8 items-center">
+          <div className="text-pink-50 text-7xl ">Wordle Game</div>
+          <div
+            className="flex flex-col gap-4 
         "
-        >
-          {Array.from({ length: tries }).map((_, idx) => (
-            <div className="flex gap-4" key={idx}>
-              {Array.from({ length: word.length }).map((_, idx) => (
-                <div
-                  key={idx}
-                  className="bg-gray-200 z-10 rounded-xl w-20 h-20"
-                ></div>
-              ))}
-            </div>
-          ))}
+          >
+            {Array.from({ length: tries }).map((_, idx1) => (
+              <div className="flex gap-4" key={idx1}>
+                {Array.from({ length: word.length }).map((_, idx2) => (
+                  <div
+                    key={idx2}
+                    className="bg-gray-200 z-10 rounded-xl w-20 h-20 flex items-center justify-center"
+                  >
+                    <div className=" text-5xl font-bold text-gray-600">
+                      {currenttry.tries === idx1 && currenttry.word[idx2]
+                        ? currenttry.word[idx2]
+                        : ""}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div
+            className="flex flex-wrap gap-2 content-start justify-evenly"
+            id="wordleboard"
+            style={{
+              width: "calc(46px * 12)",
+            }}
+          >
+            {alphabetinquerty.map((letter, idx) => (
+              <div
+                key={idx}
+                className={`${
+                  letter === "Enter" || letter == "⌫" ? "w-40" : "w-20"
+                } bg-indigo-100 z-10 rounded-xl h-16 flex items-center justify-center cursor-pointer relative hover:bg-indigo-200`}
+              >
+                <div className="absolute inset-0 flex items-center justify-center text-3xl font-bold text-gray-800">
+                  {letter}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-        {/* <div className="grid grid-cols-4 gap-4 w-max ">
-          <div
-            className={` w-20 h-20   flex items-center justify-center cursor-pointer  relative`}
-          >
-            <div className="bg-gray-200 z-10 rounded-xl absolute inset-0"></div>
-          </div>
-        </div> */}
-      </div>
-      <div
-        className="flex flex-wrap gap-2 content-start justify-evenly"
-        id="wordleboard"
-        style={{
-          width: "calc(46px * 12)",
-        }}
-      >
-        {alphabetinquerty.map((letter, idx) => (
-          <div
-            key={idx}
-            className={`${
-              letter === "Enter" || letter == "⌫" ? "w-28" : "w-20"
-            } bg-indigo-200 z-10 rounded-xl h-16 flex items-center justify-center cursor-pointer relative`}
-          >
-            <div className="absolute inset-0 flex items-center justify-center text-3xl font-bold">
-              {letter}
-            </div>
-          </div>
-        ))}
-      </div>
+      )}
     </main>
   );
 }
