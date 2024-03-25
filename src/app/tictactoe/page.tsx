c596df3496940c8a16ecfab4ce56623953991aad
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-03-25 11:37:14 +0100

    add clicking effect

--- src/app/tictactoe/page.tsx
+++ src/app/tictactoe/page.tsx
@@ -1,9 +1,60 @@
-import React from "react";
-import { chevronBack } from "../helpers/svgs";
+"use client";
+import React, { useState } from "react";
+import { chevronBack, o, x } from "../helpers/svgs";
 import Link from "next/link";
 import Svg from "../assets/Svg";
 
-export default function page() {
+export default function TicTacpage() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [turn, setTurn] = useState("X");
+  const [gameended, setGameended] = useState({
+    winner: "" as "X" | "O" | "",
+    isDraw: false,
+    score: {
+      X: 0,
+      O: 0,
+    },
+  });
+  const mark = (index: number) => {
+    if (board[index] === "" && !gameended.winner) {
+      const newBoard = [...board];
+      newBoard[index] = turn;
+      setBoard(newBoard);
+      setTurn(turn === "X" ? "O" : "X");
+      checkWinner();
+    }
+  };
+  const checkWinner = () => {
+    const winningCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningCombination.length; i++) {
+      const [a, b, c] = winningCombination[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setGameended({
+          winner: board[a] as "X" | "O",
+          isDraw: false,
+          score: {
+            ...gameended.score,
+            [board[a]]: gameended.score[board[a] as "X" | "O"] + 1,
+          },
+        });
+      }
+    }
+    if (!board.includes("")) {
+      setGameended({
+        ...gameended,
+        isDraw: true,
+      });
+    }
+  };
   return (
     <main className="h-screen flex flex-col items-center justify-around relative">
       <Link
@@ -21,33 +72,114 @@ export default function page() {
       </Link>
       <div className="flex flex-col gap-8 items-center">
         <div className="text-pink-50 text-7xl ">Tic Tac Toe Game</div>
-        <div className="grid grid-cols-3 grid-rows-3">
-          <div className="flex items-center justify-center text-4xl font-bold w-48 h-48">
-            1
+        <div className="grid grid-cols-3 grid-rows-3 w-96 h-96">
+          <div
+            className="flex items-center justify-center text-gray-50"
+            onClick={() => mark(0)}
+          >
+            {board[0] && (
+              <Svg
+                path={board[0] === "X" ? x.path : o.path}
+                view={board[0] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold border-l-2 border-r-2  border-blue-50">
-            2
+          <div
+            className="flex items-center justify-center  border-l-2 border-r-2  border-blue-50 text-gray-50"
+            onClick={() => mark(1)}
+          >
+            {board[1] && (
+              <Svg
+                path={board[1] === "X" ? x.path : o.path}
+                view={board[1] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold">
-            3
+          <div
+            className="flex items-center justify-center text-gray-50"
+            onClick={() => mark(2)}
+          >
+            {board[2] && (
+              <Svg
+                path={board[2] === "X" ? x.path : o.path}
+                view={board[2] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold border-t-2 border-b-2 border-blue-50">
-            4
+          <div
+            className="flex items-center justify-center  border-t-2 border-b-2 border-blue-50 text-gray-50"
+            onClick={() => mark(3)}
+          >
+            {board[3] && (
+              <Svg
+                path={board[3] === "X" ? x.path : o.path}
+                view={board[3] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold border-2 border-blue-50">
-            5
+          <div
+            className="flex items-center justify-center  border-2 border-blue-50 text-gray-50"
+            onClick={() => mark(4)}
+          >
+            {board[4] && (
+              <Svg
+                path={board[4] === "X" ? x.path : o.path}
+                view={board[4] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold  border-t-2 border-b-2  border-blue-50">
-            6
+          <div
+            className="flex items-center justify-center   border-t-2 border-b-2  border-blue-50 text-gray-50"
+            onClick={() => mark(5)}
+          >
+            {board[5] && (
+              <Svg
+                path={board[5] === "X" ? x.path : o.path}
+                view={board[5] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold">
-            7
+          <div
+            className="flex items-center justify-center text-gray-50"
+            onClick={() => mark(6)}
+          >
+            {board[6] && (
+              <Svg
+                path={board[6] === "X" ? x.path : o.path}
+                view={board[6] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className="flex items-center justify-center text-4xl font-bold  border-l-2 border-r-2  border-blue-50">
-            8
+          <div
+            className="flex items-center justify-center   border-l-2 border-r-2  border-blue-50 text-gray-50"
+            onClick={() => mark(7)}
+          >
+            {board[7] && (
+              <Svg
+                path={board[7] === "X" ? x.path : o.path}
+                view={board[7] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
-          <div className=" flex items-center justify-center text-4xl font-bold">
-            9
+          <div
+            className=" flex items-center justify-center text-gray-50"
+            onClick={() => mark(8)}
+          >
+            {board[8] && (
+              <Svg
+                path={board[8] === "X" ? x.path : o.path}
+                view={board[8] === "X" ? x.viewBox : o.viewBox}
+                classlist="w-20 h-20 cursor-pointer fill-current "
+              />
+            )}
           </div>
         </div>
       </div>
