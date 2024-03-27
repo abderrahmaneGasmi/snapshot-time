9139e7619470825a4e965cf8b384a6c7c8f72a63
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-03-27 10:12:52 +0100

    create the header

--- src/app/minesweeper/page.tsx
+++ src/app/minesweeper/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { chevronBack, flag, o, player1, player2, x } from "../helpers/svgs";
+import {
+  chevronBack,
+  chevrondown,
+  chevronup,
+  flag,
+  o,
+  player1,
+  player2,
+  timer,
+  x,
+} from "../helpers/svgs";
 import Link from "next/link";
 import Svg from "../assets/Svg";
 import ConfettiScreen from "../assets/ConfettiScreen";
 import { motion } from "framer-motion";
 export default function MinesWeeperpage() {
+  const [showdropdown, setShowdropdown] = useState(false);
   return (
     <main className="h-screen flex flex-col items-center justify-evenly relative">
       <Link
@@ -23,7 +34,33 @@ export default function MinesWeeperpage() {
       </Link>
       <div className="flex flex-col gap-8 items-center">
         <div className="text-pink-50 text-7xl ">Minesweeper Game</div>
-        <div className="flex gap-4">
+        <div className="flex gap-4 justify-between w-full items-center">
+          <div
+            className="relative text-pink-50 text-3xl cursor-pointer bg-indigo-900 p-2 rounded flex items-center gap-2"
+            onClick={() => setShowdropdown(!showdropdown)}
+          >
+            Easy
+            <Svg
+              path={showdropdown ? chevronup.path : chevrondown.path}
+              view={chevronup.viewBox}
+              classlist="w-6 h-6 fill-current text-pink-50"
+            />
+            {showdropdown && (
+              <div className="absolute top-full left-0 w-48 bg-indigo-600 p-2 gap-2 rounded flex flex-col ">
+                <div className="text-pink-50 text-3xl">Easy</div>
+                <div className="text-pink-50 text-3xl">Medium</div>
+                <div className="text-pink-50 text-3xl">Hard</div>
+              </div>
+            )}
+          </div>
+          <div className="flex  gap-4 items-center">
+            <Svg
+              path={timer.path}
+              view={timer.viewBox}
+              classlist="w-8 h-8 fill-current text-pink-50"
+            />{" "}
+            <div className="text-pink-50 text-3xl">00:00</div>
+          </div>
           <div className="flex  gap-4 items-center">
             <Svg
               path={flag.path}
