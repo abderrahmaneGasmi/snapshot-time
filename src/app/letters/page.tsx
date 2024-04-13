e9d716e691c79e0c8f6c3b797bcb297bf038fbcd
abderrahmaneGasmi <abderrahmen.gasmi@univ-constantine2.dz>
2024-04-13 12:26:52 +0100

    add infinite carousel letters

--- src/app/letters/page.tsx
+++ src/app/letters/page.tsx
@@ -4,6 +4,7 @@ import { chevronBack, letters } from "../helpers/svgs";
 import Link from "next/link";
 import Svg from "../assets/Svg";
 import { motion } from "framer-motion";
+import { get } from "http";
 export default function Letterspage() {
   const [Letters, setLetters] = useState([
     {
@@ -60,21 +61,10 @@ export default function Letterspage() {
   ]);
   const [current, setCurrent] = useState(Letters[5]);
   const [carousselX, setCarousselX] = useState(-300);
-  const show4letters = () => {
-    let index = Letters.indexOf(current);
-    let arr = [];
-    for (let i = index - 3; i < index + 4; i++) {
-      if (i < 0) {
-        arr.push(Letters[Letters.length + i]);
-      } else if (i >= Letters.length) {
-        arr.push(Letters[i - Letters.length]);
-      } else {
-        arr.push(Letters[i]);
-      }
-    }
-    return arr;
-  };
 
+  const getcurrentLetteridx = () => {
+    return Letters.indexOf(current);
+  };
   const handleLetter = (letter: { letter: string; position: number }) => {
     // setCurrent(letter);
     let index = Letters.indexOf(letter);
@@ -84,7 +74,6 @@ export default function Letterspage() {
     }
 
     let diff = currentindex - index;
-    console.log(diff);
     setCarousselX((prev) => {
       return prev + diff * 100;
     });
@@ -103,33 +92,42 @@ export default function Letterspage() {
           }
         }
       });
+      if (index === 0) {
+        arr[arr.length - 1].position = 10;
+        arr[arr.length - 2].position = -10;
+      }
+      if (index === arr.length - 1) {
+        arr[0].position = 10;
+        arr[1].position = -10;
+      }
+      if (index === 1) {
+        arr[arr.length - 1].position = -10;
+      }
+      if (index === arr.length - 2) {
+        arr[0].position = -10;
+      }
+      if (index === 2) {
+        // make the last item in first position
+        arr.unshift({ letter: arr[arr.length - 1].letter, position: 20 });
+
+        arr.pop();
+        setCarousselX(-100);
+      }
+      if (index === arr.length - 3) {
+        // make the first item in last position
+        arr.push({ letter: arr[0].letter, position: 20 });
+        arr.shift();
+        setCarousselX(-600);
+      }
       return arr;
     });
   };
-
   //   useEffect(() => {
-  //     const handleKey = (e: KeyboardEvent) => {
-  //       if (e.key === "ArrowRight") {
-  //         let index = Letters.indexOf(current);
-  //         if (index === Letters.length - 1) {
-  //           setCurrent(Letters[0]);
-  //         } else {
-  //           setCurrent(Letters[index + 1]);
-  //         }
-  //       } else if (e.key === "ArrowLeft") {
-  //         let index = Letters.indexOf(current);
-  //         if (index === 0) {
-  //           setCurrent(Letters[Letters.length - 1]);
-  //         } else {
-  //           setCurrent(Letters[index - 1]);
-  //         }
-  //       }
-  //     };
-  //     window.addEventListener("keydown", handleKey);
-  //     return () => {
-  //       window.removeEventListener("keydown", handleKey);
-  //     };
-  //   }, [Letters, current]);
+  //     if (getcurrentLetteridx() === 3) {
+  //       console.log("first");
+  //       setCarousselX(-100);
+  //     }
+  //   }, [current]);
 
   return (
     <main className="h-screen flex flex-col items-center justify-evenly relative select-none">
@@ -188,7 +186,7 @@ export default function Letterspage() {
             duration: 0.5,
           }}
         >
-          {Letters.map((letter, index) => (
+          {/* {showprevcaroussel().map((letter, index) => (
             <motion.div
               key={index}
               className={`text-7xl p-8 rounded cursor-pointer ${
@@ -216,7 +214,66 @@ export default function Letterspage() {
             >
               {letter.letter}
             </motion.div>
+          ))} */}
+
+          {Letters.map((letter, index) => (
+            <motion.div
+              key={index}
+              className={`text-7xl p-8 rounded cursor-pointer ${
+                letter.position === 10
+                  ? "text-gray-800  bg-gray-700"
+                  : letter.position === -10
+                  ? "text-gray-700  bg-gray-400"
+                  : letter.position === -50
+                  ? "text-gray-600 bg-white "
+                  : "text-gray-800  bg-gray-800"
+              }`}
+              onClick={() => handleLetter(letter)}
+              initial={{
+                y: letter.position,
+              }}
+              animate={{
+                y: letter.position,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              whileHover={{
+                scale: 1.1,
+              }}
+            >
+              {letter.letter}
+            </motion.div>
           ))}
+          {/* {shownextcaroussel().map((letter, index) => (
+            <motion.div
+              key={index}
+              className={`text-7xl p-8 rounded cursor-pointer ${
+                letter.position === 10
+                  ? "text-gray-800  bg-gray-700"
+                  : letter.position === -10
+                  ? "text-gray-700  bg-gray-400"
+                  : letter.position === -50
+                  ? "text-gray-600 bg-white "
+                  : ""
+              }`}
+              onClick={() => handleLetter(letter)}
+              initial={{
+                y: letter.position,
+              }}
+              animate={{
+                y: letter.position,
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+              whileHover={{
+                scale: 1.1,
+              }}
+            >
+              {letter.letter}
+            </motion.div>
+          ))} */}
         </motion.div>
       </div>
     </main>
